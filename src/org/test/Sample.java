5a9924d6a31f28bc562d7380cfcf74eee621e1e4
Gowthamgtm05 <gowthamgtms03@gmail.com>
2021-07-07 17:48:33 +0530

    Soundar commits

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,5 +1,14 @@
 package org.test;
 
 public class Sample {
+	
+	
+	public static void login() {
+		System.out.println("Login to click the button");
+	}
 
+	
+	public static void main(String[] args) {
+		login();
+	}
 }
